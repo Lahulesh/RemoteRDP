0f90caa554283ae0975b45fea4179fac10d9d8bd
Lahulesh <lahuleshchourasiya2000@gmail.com>
2023-08-06 21:54:22 +0530

    My First RDP Application

---
+++ RDP/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace RDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMsRdpClient6NotSafeForScripting1 = new AxMSTSCLib.AxMsRdpClient6NotSafeForScripting();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axMsRdpClient6NotSafeForScripting1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMsRdpClient6NotSafeForScripting1
+            // 
+            this.axMsRdpClient6NotSafeForScripting1.Enabled = true;
+            this.axMsRdpClient6NotSafeForScripting1.Location = new System.Drawing.Point(12, 52);
+            this.axMsRdpClient6NotSafeForScripting1.Name = "axMsRdpClient6NotSafeForScripting1";
+            this.axMsRdpClient6NotSafeForScripting1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMsRdpClient6NotSafeForScripting1.OcxState")));
+            this.axMsRdpClient6NotSafeForScripting1.Size = new System.Drawing.Size(1294, 615);
+            this.axMsRdpClient6NotSafeForScripting1.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(1071, 23);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(1170, 23);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(86, 23);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1318, 679);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.axMsRdpClient6NotSafeForScripting1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axMsRdpClient6NotSafeForScripting1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsRdpClient6NotSafeForScripting axMsRdpClient6NotSafeForScripting1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+    }
+}
+
---
+++ RDP/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            axMsRdpClient6NotSafeForScripting1.Server = "192.168.0.105";
+            axMsRdpClient6NotSafeForScripting1.UserName = "Administor";
+            //axMsRdpClient6NotSafeForScripting1.AdvancedSettings2.ClearTextPassword = "pass@123";
+            axMsRdpClient6NotSafeForScripting1.AdvancedSettings7.EnableCredSspSupport = true;
+
+            axMsRdpClient6NotSafeForScripting1.ColorDepth = 16;
+            axMsRdpClient6NotSafeForScripting1.DesktopHeight = 720;
+            axMsRdpClient6NotSafeForScripting1.DesktopWidth = 1024;
+            axMsRdpClient6NotSafeForScripting1.Connect();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if(axMsRdpClient6NotSafeForScripting1.Connected.ToString() == "1")
+            {
+                axMsRdpClient6NotSafeForScripting1.Disconnect();
+            }
+        }
+    }
+}
